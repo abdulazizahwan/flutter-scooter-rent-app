3c2e40b96146e481ab97b24a95f2837703e64b43
abdulazizahwan <abdulazizahwan97@gmail.com>
2023-12-14 20:05:52 +0700

    upload part 2: order scooter page

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_scooter_rent_app/order_scooter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: OrderScooterPage(),
     );
   }
 }
